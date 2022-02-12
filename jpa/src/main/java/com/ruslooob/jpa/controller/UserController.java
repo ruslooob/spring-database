1a5dff1cd79abffcca28688f32168f22608b1a2c
Ruslan Mamedov <rm952576@gmail.com>
2022-02-12 20:26:19 +0300

    add README.md

---
+++ jpa/src/main/java/com/ruslooob/jpa/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ruslooob.jpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello world";
+    }
+}
